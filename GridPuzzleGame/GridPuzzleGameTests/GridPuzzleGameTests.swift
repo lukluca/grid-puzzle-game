5a2bc3d06075fb982847342e157262d6a4e7191b
Luca Tagliabue <lu.tagliabue@reply.it>
2026-01-30 19:08:04 +0100

    Initial commit - Project setup

---
+++ GridPuzzleGame/GridPuzzleGameTests/GridPuzzleGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  GridPuzzleGameTests.swift
+//  GridPuzzleGameTests
+//
+//  Created by lukluca on 25/01/26.
+//
+
+import Testing
+@testable import GridPuzzleGame
+
+struct GridPuzzleGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
